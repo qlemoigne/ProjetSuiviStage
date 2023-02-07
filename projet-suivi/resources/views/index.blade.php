3dbecf504d7704d05b310b64729a45fafa6b6dfe
Robin <bukielskirobin@gmail.com>
2023-02-07 17:23:28 +0100

    te

--- projet-suivi/resources/views/index.blade.php
+++ projet-suivi/resources/views/index.blade.php
@@ -2,7 +2,7 @@
 <html>
     <head>
     @vite(['resources/css/app.css'])
-    <script src='resources/js/app.js'></script>
+    <script src='/app.js'></script>
     </head>
     <body>
        <h1>page d'accueil du site web</h1>
