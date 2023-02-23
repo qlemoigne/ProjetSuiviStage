a193b42f48da331b3f770aabbd518a659615817b
Robin <bukielskirobin@gmail.com>
2023-02-23 15:28:56 +0100

    seeder

---
+++ laravelsuivi8/database/seeders/Participe.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class Participe extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('participation')->insert([
+           'id'=> 1,
+            'utilisateurs_id'=>1,
+            'activites_id'=>1,
+        ],
+        [
+            'id'=> 2,
+             'utilisateurs_id'=>2,
+             'activites_id'=>1,
+         ]);
+       
+    }
+}
+
+
---
+++ laravelsuivi8/database/seeders/Utilisateur.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class Utilisateur extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('utilisateurs')->insert([
+           'id'=> 1,
+            'nom'=>'bukielski',
+            'prenom'=>'Robin',
+            'telephone'=>'0782051968',
+            'mail'=>'bukielskirobin@gmail.com',
+            'profil'=>'1',
+
+        ],
+        [
+            'id'=> 2,
+            'nom'=>'seneclauze',
+            'prenom'=>'baptiste',
+            'telephone'=>'0782051968',
+            'mail'=>'seneclauzebaptiste@gmail.com',
+            'profil'=>'1',
+        ]);
+    }
+}
+
