1829e2404ae58b3ee0dd9ec39fa261b3099dcea5
TheDadi59 <di.danielnd59860@gmail.com>
2023-03-01 10:57:46 +0100

    Commit correction accueil

--- laravelsuivi8/resources/views/accueil.blade.php
+++ laravelsuivi8/resources/views/accueil.blade.php
@@ -9,35 +9,37 @@
         </div>
     </div>
     <table id="table_accueil">
-    <thead>
-    <tr>
-      <th scope="col">Type d'activité</th>
-      <th scope="col">Nom de l'étudiant  </th>
-      <th scope="col">Thématique</th>
-      <th scope="col">Nom du tuteur  </th>
-      <th scope="col">Début de l'activité</th>
-      <th scope="col">Fin de l'activité</th>
-
-    </tr>
-  </thead>
+        <thead>
+        <tr>
+            <th scope="col">Type d'activité</th>
+            <th scope="col">Nom de l'étudiant  </th>
+            <th scope="col">Thématique</th>
+            <th scope="col">Nom du tuteur  </th>
+            <th scope="col">Début de l'activité</th>
+            <th scope="col">Fin de l'activité</th>
+        </tr>
+    
+    </thead>
             @foreach($utilisateur->activites as $activite)
             
            
             <tr onclick="window.location.assign('{{ route('activite', ['id'=> $activite->id]) }}');"> 
              
                 <td>{{$activite->types->libelle}}</td>
-                <td>{{$activite->debut}} </td>
-                <td>{{$activite->fin}}</td>
-                <td>
-                
+                <td> 
                 @foreach($activite->utilisateurs as $u)
-              
-                    @if($u->id != $utilisateur->id) 
+                    @if($u->id != $utilisateur->id)
+                    <a href="mailto:{{$u->prenom}}.{{$u->nom}}@etu.imt-nord-europe.fr"> 
                     {{$u->prenom}}  
                     {{$u->nom}}
                      @endif 
                 @endforeach
                 </td>
+                <td> {{$activite->thematique}} </td>
+                <td><a href="mailto:{{$activite->adresse_mail_tuteur_externe}}">{{$activite->nom_tuteur_externe}}</td>
+                <td>{{$activite->debut}} </td>
+                <td>{{$activite->fin}}</td>
+
             </tr>
 
             @endforeach
